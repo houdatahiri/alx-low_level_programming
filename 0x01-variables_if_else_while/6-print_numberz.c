a5f9d580234111bd963255b669afd3af59a1ec5b
houdatahiri <hudatahirialaoui@gmail.com>
2023-06-16 16:46:33 -0700

    c variable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i=48; i<58; i++)
+		putvhar(i);
+	putchar('\n');
+	return (0);
+}
