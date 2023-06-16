689cd0b2ec4dce2b365d3f34e916665199c4a2dc
houdatahiri <hudatahirialaoui@gmail.com>
2023-06-16 16:52:04 -0700

    c variable

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "zyxwutsrqponmlkjhigfedcba";
+	int i;
+	for (i = 0; i < 26; i++)
+		putchar(alp[i]);
+	putchar('\n');
+	return (0);
+}
