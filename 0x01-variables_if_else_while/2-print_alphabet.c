992a078c4e848e79954f5dda3981ee4a9c535339
houdatahiri <hudatahirialaoui@gmail.com>
2023-06-16 16:16:54 -0700

    c variable

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,9 @@
  * Return: Always 0 (Success)
  */
 int main (void){
-        char alp[26] = "abcdefghijklmnopqrstuv";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-       	for (i=0; i<26; i++)
+       	for (i = 0; i < 26; i++)
 		putchar(alp[i]);
 	putchar('\n');
 	return (0);
