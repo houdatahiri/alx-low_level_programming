c8a09d845f6e96d1964ec61527895fd02795b2c4
houdatahiri <hudatahirialaoui@gmail.com>
2023-06-20 14:04:52 -0700

    c function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
+
 /**
- * _isalpha - checks for c or C
- * @c : is the character
- * return 1 if c is a letter otherwise return 0
- */
+* _isalpha - checks for alphabetic character
+* @c: the character to be checked
+* Return: 1 if c is a letter, 0 otherwise
+*/
 
 int _isalpha(int c)
 {
