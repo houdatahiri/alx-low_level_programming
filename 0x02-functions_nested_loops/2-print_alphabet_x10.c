47355cf0c4f9f0ff0a80e7cd7b6acf751403a0a0
houdatahiri <hudatahirialaoui@gmail.com>
2023-06-20 13:23:52 -0700

    c function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 
 void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
-	
 	i = 0;
 	while (i < 10)
 	{
