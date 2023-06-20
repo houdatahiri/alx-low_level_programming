3a08094572b7bb9b6b3aa3a434133a095f459e37
houdatahiri <hudatahirialaoui@gmail.com>
2023-06-20 14:02:01 -0700

    c function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - function 
+ * _isalpha - function
  * @c : is the character
  * return 1 if c is a letter otherwise return 0
  */
@@ -9,8 +9,5 @@
 int _isalpha(int c)
 {
 
-	if (c == 'c' || c == 'C')
-		return (1);
-	else
-		return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
