4fa17eba1501e58ca734b1959825ca9986f22cbd
houdatahiri <hudatahirialaoui@gmail.com>
2023-06-20 04:32:40 -0700

    c funtion

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++){
+		_putchar(alp);
+		_putchar('\n');
+	}
+}
