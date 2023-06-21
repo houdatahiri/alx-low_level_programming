24606fd6d801f0363887bf56edd3e3e28f3298c3
houdatahiri <hudatahirialaoui@gmail.com>
2023-06-20 17:03:32 -0700

    c function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,9 +9,8 @@
 
 void print_to_98(int n)
 {
-	int i;
-	int j;
-	
+	int i, j;
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
@@ -23,23 +22,20 @@ void print_to_98(int n)
 			else if (i == 98)
 			{
 				printf("%d\n", i);
-		
 			}
 		}
+		else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != 98)
+				{
+					printf("%d, ", j);
+				}
+				else if (j == 98)
+				{
+					printf("%d\n", j);
+				}
+			}
 	}
-	else if (n >= 98)
-	{
-		for (j = n; j >= 98; j++)
-                {
-                        if (j != 98)
-			{
-                                printf("%d,", j);
-			}
-			else if (j == 98)
-			{
-		       		printf("%d\n ", j);
-                
-			}
-		}
-        }
 }
