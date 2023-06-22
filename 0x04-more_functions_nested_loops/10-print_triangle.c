9060c398a7f4baef8a30b58ef960890f9373e940
houdatahiri <hudatahirialaoui@gmail.com>
2023-06-22 05:16:36 -0700

    0x04

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,20 +11,22 @@ void print_triangle(int size)
 	}
 	else
 	{
-		int i;
-		int j;
+		int i, j;
 
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < size; j++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
+
 			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
+
 			_putchar('\n');
 		}
 	}
 }
+
