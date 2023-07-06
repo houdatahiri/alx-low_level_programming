baaace5a046a7bb054a0c4d90d8558ac328e1e30
houdatahiri <hudatahirialaoui@gmail.com>
2023-07-05 17:38:16 -0700

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: the string to be measurd
- * return: the length of the string
+ * Return: the length of the string
  */
 int _strlen_recursion(char *s)
 {
