e2536f8148777825f12c90c507d8ac9e07e99a5b
houdatahiri <hudatahirialaoui@gmail.com>
2023-07-11 16:52:57 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * count_word - helper function to count the number of words in a string 
+ * count_word - helper function to count the number of words in a string
  * @s: string to evaluate
  * Return: number of words
  */
