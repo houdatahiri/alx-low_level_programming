a1f2b01c2eab9cfc95ab034e2f3bbf95376d2bd0
houda <tahirialaouihouda@gmail.com>
2023-07-31 15:50:15 -0700

    more linked

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sums_listint - calcluate the sum
+ * @head: first node
+ * Return: resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
