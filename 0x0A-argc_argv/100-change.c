dd3322c5bbd7af91ba0643e648b959f08406ee30
houda <tahirialaouihouda@gmail.com>
2023-10-02 14:17:04 -0700

    rgv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - prints the min number
+ * @argc: number
+ * @argv: array
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Erro_n");
+		return (0);
+	}
+	num = atoi(argv[1]);
+	result = 0;
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
