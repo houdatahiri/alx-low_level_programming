fb59306cf6a9c6b599f187a7cc857c2b7cabafe3
houda <tahirialaouihouda@gmail.com>
2023-10-19 07:17:43 -0700

    linked

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len - returns the number
+ * @h: pointer
+ * Return: number
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+readme file of the directory 0x12-singly_linked_lists
