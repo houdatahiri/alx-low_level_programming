47c0a96aeaa7274ae9c9ec192e701f3139f23be9
houda <tahirialaouihouda@gmail.com>
2023-11-03 06:00:08 -0700

    shell

---
+++ simple_shell/file_function.c
@@ -0,0 +1,66 @@
+#include "shell.h"
+/**
+ * _strlen - string length
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * starts_with - checker
+ * @haystack: string
+ * @needle: substring
+ * Return: address
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+/**
+ * _strcmp - comparison of 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: negative, positive or zero
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+/**
+ * _strcat - concatenates 2 stringd
+ * @dest: destination
+ * @src: source
+ * Return: pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
