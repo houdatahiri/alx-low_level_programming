3ef7c27c5ba05d8f80c12c864f84b495644e04a9
houda <tahirialaouihouda@gmail.com>
2023-12-07 11:49:39 -0800

    double

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns node
+ * @head: pointer
+ * @index: index
+ * Return: address
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sums the value
+ * @head: pointer
+ * Return: int sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
