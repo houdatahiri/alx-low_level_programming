f87025d486b9d71a55bf7ebb9e0ad8b7c944a1c9
houda <tahirialaouihouda@gmail.com>
2023-12-07 09:04:58 -0800

    doubly

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * orint_dlistint - prints doubly_linked list
+ * @h: address
+ * Return: size of list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+readme file of 0x17-doubly_linked_lists
