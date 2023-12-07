05a343c5e35b3572f8dc180e11f5dee15c80b20f
houda <tahirialaouihouda@gmail.com>
2023-12-07 11:32:11 -0800

    double

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlist
+ * @head: pointer
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
