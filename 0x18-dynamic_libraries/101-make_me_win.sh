2481ef16422bb6f81e7a555002fd6331cfff778b
houda <tahirialaouihouda@gmail.com>
2023-12-11 16:20:10 -0800

    dyn

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/houdaalaoui/alx-low_level_programming/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/houdatahiri/alx-low_level_programming/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
